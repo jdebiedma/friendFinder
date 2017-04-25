ccee0f817bdeeeafc362f1714416f7bdf7d94454
Juan DeBiedma <juanthefatty@gmail.com>
2017-04-25 02:49:26 -0400

    first draft

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+var friendArray = [
+
+{
+  "name":"Ahmed",
+  "photo":"https://pbs.twimg.com/profile_images/489700476749770754/GlukBJRJ_400x400.jpeg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+
+{
+  "name":"Patrick",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png",
+  "scores":[
+      2,
+      5,
+      4,
+      3,
+      5,
+      1,
+      3,
+      5,
+      1,
+      5
+    ]
+},
+
+{
+  "name":"Susan",
+  "photo":"http://www.books-n-kisses.com/wp-content/uploads/2013/03/MBeckett_cred-Susan-A-Abt-Photography.jpg",
+  "scores":[
+      4,
+      4,
+      4,
+      5,
+      1,
+      2,
+      3,
+      1,
+      1,
+      1
+    ]
+},
+
+{
+  "name":"Juan",
+  "photo":"https://www.ssbwiki.com/images/5/52/Hungrybox_teamliquid.jpg",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+}
+
+];
+
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,68 @@
+// Load Data - Link to friendsData array in JS data source
+
+var friendsData = require("../data/friends");
+
+// Routing 
+
+module.exports = function(app) {
+
+// API Get Requests
+// Determines what the program will fetch when certain URLs are typed in
+
+ app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+
+app.post("/api/friends", function(req, res) {
+
+	friendsData.push(req.body);
+	//res.json("Updated!");
+	//res.json(friendsData);
+
+	//inside here do compatability logic
+	//mathematics, etc
+	//first off make sure that the friends are added in 
+
+	var newData = friendsData[friendsData.length - 1];
+	var newDataScores = newData.scores;
+
+	var compArray = [];
+
+	var newNumbah = parseInt(newData.scores[0]) - parseInt(friendsData[0].scores[0]);
+
+	var numberX = 0;
+	var myMatch = 0;
+
+	
+
+	for (var i = 0; i < friendsData.length - 1; i++) {
+
+			for (var j = 0; j < 10; j++) {
+
+				var addMe = Math.abs( parseInt(newData.scores[j]) - parseInt(friendsData[i].scores[j]) );
+
+				numberX = numberX + addMe;
+
+				
+				
+			}
+				compArray.push(numberX);
+				numberX = 0;
+				
+				
+
+	}
+
+	myMin = Math.min.apply(Math, compArray);
+	myMatch = compArray.indexOf(myMin);
+
+	
+
+	res.json("Your match is " + friendsData[myMatch].name);
+
+});
+
+
+};
+
